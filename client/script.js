8a232cb0a2ca9558890ada2752ff15ad7b50bbd3
Dangarang <dangto@vt.edu>
2024-11-22 15:34:52 -0500

    removed some code to make it work

--- client/script.js
+++ client/script.js
@@ -21,33 +21,16 @@ canvas.addEventListener('mousemove', (event) => {
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
 
+    //Cool line styling yippee
     ctx.lineWidth = 2;
     ctx.lineCap = 'round';
-    ctx.strokeStyle = 'black';
+    ctx.strokeStyle = 'white';
 
     ctx.lineTo(x, y);
-    ctx.gstroke();
+    ctx.stroke();
     ctx.beginPath();
     ctx.moveTo(x, y);
 
     // Emit drawing data to server
     sendDrawingData({ x, y });
 });
-
-// WebSocket connection
-const socket = new WebSocket('ws://localhost:3000');
-
-// Send drawing data
-function sendDrawingData(position) {
-    socket.send(JSON.stringify(position));
-}
-
-// Receive drawing data
-socket.onmessage = (message) => {
-    const position = JSON.parse(message.data);
-
-    ctx.lineTo(position.x, position.y);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(position.x, position.y);
-};
