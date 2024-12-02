7c5345d31c4a1a81ff817f1c51caf62edcd7f05d
Andar Manik <59768374+AndarManik@users.noreply.github.com>
2024-12-02 00:05:29 -0500

    comment

--- client/client.js
+++ client/client.js
@@ -8,6 +8,8 @@ import Pencil from "./pencil.js";
 const pencil = new Pencil(virtualCanvas, colorpicker);
 
 //CALM is this the correct location for this?
+
+// AGI: This should get turned into a little class like color picker, eventually if enough of these little classes come around we will make a class that has them all as field members
 const brushSizeDropdown = document.getElementById('default-sizes');
   brushSizeDropdown.addEventListener('change', (event) => {
     const newBrushSize = parseInt(event.target.value, 10);
@@ -20,7 +22,8 @@ new Input(pencil);
 //BUILD replace this with yours
 // http://localhost:3000/
 // wss://5r83l7fz-3001.use.devtunnels.ms/
-const ws = new WebSocket("http://localhost:3001/");
+
+const ws = new WebSocket("wss://5r83l7fz-3001.use.devtunnels.ms/");
 
 // When a message is sent to this client it is received here
 ws.onmessage = (event) => {
