8820aa72e33f4bb7494be3ee362f17432961bdb0
AlanDorn <acdorn00@gmail.com>
2024-12-03 19:20:52 -0500

    alt shift f

--- client/input.js
+++ client/input.js
@@ -10,31 +10,26 @@ export default class Input {
       ); // try CNTRL-SHIFT-I
     });
 
-
     //maybe switch to a swith case statement
     document.addEventListener("mousedown", (event) => {
-      if (event.button === 0) {   
-        console.log("left click");           //0 = left, 1 = middle, 2 = right
+      if (event.button === 0) {
+        console.log("left click"); //0 = left, 1 = middle, 2 = right
         this.mouseDown = true;
         pencil.mouseDownLeft(this);
-        
       }
       if (event.button === 2) {
         console.log("right click");
         this.mouseDown = true;
         pencil.mouseDownRight(this);
-        
       }
     });
 
     document.addEventListener("mouseup", () => {
-      
-        this.mouseDown = false;
-        pencil.mouseUpLeft(this);
-      
-        this.mouseDown = false;
-        pencil.mouseUpRight(this);
-      
+      this.mouseDown = false;
+      pencil.mouseUpLeft(this);
+
+      this.mouseDown = false;
+      pencil.mouseUpRight(this);
     });
     document.addEventListener("mousemove", (event) => {
       this.x = event.clientX;
