c7489496ce6ed53975e7126d5d6f1df0c92c1f6b
Andar Manik <59768374+AndarManik@users.noreply.github.com>
2024-12-27 19:20:40 -0500

    I fixed some part of the pencil drawing where it didn't reach your cursor while you are drawing.

--- client/input.js
+++ client/input.js
@@ -12,7 +12,8 @@ export default class Input {
     });
 
     document.addEventListener("pointerdown", (event) => {
-      if (event.target === canvas) {//check so that click is on canvas, if on menu or outside stop beotch!
+      if (event.target === canvas) {
+        //check so that click is on canvas, if on menu or outside stop beotch!
         if (
           (event.button === 0 && event.pointerType === "mouse") ||
           event.pointerType === "touch"
@@ -33,12 +34,16 @@ export default class Input {
     });
 
     document.addEventListener("pointerup", () => {
+      clearTimeout(debounceTimeout);
       this.mouseDown = false;
       toolbar.mouseUpLeft(this);
       toolbar.mouseUpRight(this);
     });
 
+    let debounceTimeout;
+
     document.addEventListener("pointermove", (event) => {
+      clearTimeout(debounceTimeout);
       this.x = Math.round(event.clientX);
       this.y = Math.round(event.clientY);
 
@@ -49,10 +54,17 @@ export default class Input {
 
       // 1. When the cpu of the client is slow there can be glitching when the mousemove listener is allowed to call the move function everytime it can. Essentially the process queue gets backed up because all of the mouse render commands.
 
+      // 2. because of mouse move works you can get it so that you mouse stops but the pencil line isn't under the cursor. It waits until the mouse either moves again or lift to complete.
+
       if (performance.now() - lastMove > 32) {
         toolbar.mouseMove(this);
         lastMove = performance.now();
       }
+
+      debounceTimeout = setTimeout(() => {
+        toolbar.mouseMove(this);
+        lastMove = performance.now();
+      }, 32);
     });
   }
 }
